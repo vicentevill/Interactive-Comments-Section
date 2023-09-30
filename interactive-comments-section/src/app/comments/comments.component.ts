919c1747b0ebbd3c3dd867359eba276fef81fcef
thekingvice <villarrealvicente92@gmail.com>
2023-09-29 17:16:37 -0700

    commit

--- interactive-comments-section/src/app/comments/comments.component.ts
+++ interactive-comments-section/src/app/comments/comments.component.ts
@@ -21,12 +21,12 @@ export class CommentsComponent implements OnInit {
     comment.score--;
   }
 
-  addReply(comment: any) {
+  addReply(comment: any, username: string) {
     const newComment = {
       id: 100,
-      content: 'Works',
+      content: `@${username}`,
       createdAt: '1 second ago',
-      score: 12,
+      score: 0,
       user: {
         image: this.data.currentUser.image,
         username: this.data.currentUser.username,
